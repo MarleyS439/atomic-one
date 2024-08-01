ac12c896f1e3b053df506068cfa327389c09c55b
MarleyS439 <marleysantos439@gmail.com>
2024-08-01 00:52:53 -0300

    Inserção de meta tags da Meta e demais meta tags. Melhorias nos ícones, sendo convertidos para '.ico'

--- index.php
+++ index.php
@@ -1,15 +1,40 @@
 <!DOCTYPE html>
 <html lang="pt-br" dir="ltr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="author" content="Marley de Sousa Santos">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <!-- Meta tags inicias - Meta -->
+    <meta property="og:url" content="">
+    <meta property="og:type" content="">
+    <meta property="og:title" content="">
+    <meta property="og:description" content="">
+    <meta property="og:image" content="">
+    <meta property="og:site_name" content="">
+    <meta property="og:locale" content="">
+
     <title>Atomic</title>
 </head>
+
 <body>
-    <header></header>
+    <header>
+        <!-- Navbar -->
+        <nav></nav>
+
+    </header>
 
     <main></main>
 
-    <footer></footer>
+    <footer>
+        
+        <!-- Copyright -->
+        <div class="copy"></div>
+    </footer>
 </body>
+
 </html>
